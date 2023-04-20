7caf7b27d39a244f672c2b375dfbf50a228bbc26
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-04-20 21:24:55 +0000

    feat: Wire up APIs in connection to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# imported libraries
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('sell_estate')
+
+estate = SHEET.worksheet('estate')
+sold = SHEET.worksheet('sold')
+
+data = estate.get_all_values()
+
+print(data)
